9c978e50dc3dfa449e110c59dd136ff210f4eb6f
danmesfin <danielmsfn@gmail.com>
2023-02-02 01:44:27 +0300

    updated book_id to id

--- src/books/dto/book.dto.ts
+++ src/books/dto/book.dto.ts
@@ -7,3 +7,17 @@ export class BookDto {
   readonly pages: string;
   readonly img_url: string;
 }
+
+/*
+CREATE TABLE books (
+    ID int NOT NULL,
+      title varchar(255),
+  genre varchar(255),
+   description varchar(255),
+  author varchar(255),
+   publisher varchar(255),
+  pages varchar(255),
+  img_url varchar(255),
+    PRIMARY KEY (ID)
+);
+*/
